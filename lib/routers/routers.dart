849badd04a203abc21b0c332f26e6a6c0615e2f4
Anna Verhovaya <99863500+AnnaVerhovaya@users.noreply.github.com>
2023-06-22 17:16:06 +0300

    Delete lib/routers directory

--- lib/routers/routers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_application_test_my_app/features/cart/cart_screen.dart';
-import 'package:flutter_application_test_my_app/features/dishes/category_screen.dart';
-import 'package:flutter_application_test_my_app/features/dishes/dishes_screen.dart';
-
-final routes = {
-  '/': (context) => const CategoryScreen(),
-  'dishes': (context) => const DishesScreen(),
-  'cart': (context) => const CartScreen(),
-};
